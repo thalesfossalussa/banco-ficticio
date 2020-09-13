f9e61b9425ef4438e0e113caada1385c7276a43b
Joao.grilo <joaoplopescg@gmail.com>
2020-09-13 19:47:26 -0300

    add. verifica_cpf
    isso que tu leu

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void printMenu(int * select) {
 
@@ -12,6 +13,50 @@ void printMenu(int * select) {
 	}
 }
 
+int verifica_cpf(int *cpf){
+    int c, k=10, d1=0, d2=0;
+    
+    for(c=0;c<9;c++){
+        d1=d1+cpf[c]*k;
+        k--;
+    }
+    d1=(d1*10)%11;
+    k=11;
+    if(d1==cpf[9]){
+        for(c=0;c<10;c++){
+            d2=d2+cpf[c]*k;
+            k--;
+        }
+        d2=(d2*10)%11;
+        if(d2==cpf[10]){
+            printf("cpf valido\n");
+            return 1;
+        } else {
+            printf("cpf invalido\n");
+            return 0;
+        }
+    } else {
+        printf("cpf invalido\n");
+        return 0;
+    }
+}
+
+int verifica(){
+    int c;
+    
+    int *cpf;
+    cpf=(int*)calloc(11,sizeof(int));
+        
+    for(c=0;c<11;c++){
+        scanf("%d", &cpf[c]);
+    }
+    verifica_cpf;
+    
+    free(cpf);
+    
+    return 0;
+}
+
 int main() {
 	int select = 0;
 	printMenu(&select);	
@@ -20,7 +65,8 @@ int main() {
 		case 1:
 		printf("Foi escolhido o 1\n");
 	break;
-		case 2:
+        case 2:
+            verifica;
 		printf("Foi escolhido o 2\n");
 	break;
 		case 3:
